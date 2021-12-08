b513a3792b083cc59f3e3c792798478100e46254
Michael Z Daryabeygi <ginkgomzd@fastmail.com>
2021-12-08 10:43:48 -0500

    use square, not curl for php8; fixes #4

--- nickfix.civix.php
+++ nickfix.civix.php
@@ -166,7 +166,7 @@ function _nickfix_civix_find_files($dir, $pattern) {
     if ($dh = opendir($subdir)) {
       while (FALSE !== ($entry = readdir($dh))) {
         $path = $subdir . DIRECTORY_SEPARATOR . $entry;
-        if ($entry{0} == '.') {
+        if ($entry[0] == '.') {
         }
         elseif (is_dir($path)) {
           $todos[] = $path;
